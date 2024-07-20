7afac4e9ef2d4cf69c37eac3cad40a8e00eafd6d
OksanaLynnyk <119429489+OksanaLynnyk@users.noreply.github.com>
2024-07-20 21:01:34 +0300

    fixed components

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,4 +1,4 @@
-import Transaction from "./Transaction"
+import Transaction from "../Transaction/Transaction"
 import css from './TransactionHistory.module.css'
 
 const TransactionHistory = (({items}) => {
